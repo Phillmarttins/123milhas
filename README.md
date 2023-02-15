7c317fa117557b47ae4022913d3aaaaf74408e8e
Phillmarttins <66274001+Phillmarttins@users.noreply.github.com>
2023-02-15 20:49:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 123milhas
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-h3nmqv)
---
+++ script.js
@@ -0,0 +1,57 @@
+1+1+1+1+1++1+1
+
+
+Prezado Atendimento,
+
+Favor cancelar esta solicitação https://123milhas.com/admin/order/54131640 pelo motivo de CPF divergente.
+
+
+
+https://123milhas.com/admin/order/60164869
+
+1.1 
+ 2º: AP 30
+  3º:aprovado Regra 1.1, com peso 25 índices  2 3 5 7 9 10 16 18 36 43 45 48 
+  4º: R$ 19.495,05	12x
+  
+  -ap 30
+sem alertas
+   -trecho e ip condiz
+  - numero confirmado
+  -e-mail bom em nome do TT
+  -e-mail autenticado Data: Data:  05/05/2022 15:38
+  -Titular sócio em empresa ativa
+  -Telefone: Confirmado
+  -pax mesmo sobrenome
+  -CPF do pax no bureuax do TT 
+  -Vinculo no bureuax
+  -Mesmo endereço
+  -Perfil familiar
+ Clearsale 35,95  kuntudo 0.16  
+
+
+
+
+ https://123milhas.com/admin/order/60173930
+ 
+
+ 1.1
+ 2º: AP 48
+  3º:aprovado Regra 1.1, com peso 31 índices  1 2 3 5 16 18 23 25 27 29 31 32 43  
+  4º: R$ R$ 31.892,95	12
+  -ap 48
+
+   -trecho e ip condiz
+  - numero confirmado
+  -e-mail bom em nome do TT
+  -e-mail autenticado Data:  21/05/2022 20:08
+  -Titular sócio em empresa ativa
+  -pax mesmo sobrenome
+  -Vinculo no bureuax
+  -Perfil familiar
+  compra anterior aprovado porem cancelada pelo Sistema 
+ mesmo ip mesmo dispositivo 
+
+
+
+
